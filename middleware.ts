d49595bb8bd509238323ce4b3fec8450e55ffedf
Shivang <shivangtips@gmail.com>
2024-12-25 20:14:36 +0530

    Removed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,33 @@
-import { NextResponse } from 'next/server';
-import { NextRequest } from 'next/server';
+// import { NextResponse } from 'next/server';
+// import { NextRequest } from 'next/server';
 
-const botUserAgents = [
-  'Twitterbot',
-  'Googlebot',
-  'Bingbot',
-  'FacebookExternalHit',
-  // Add any other bot user agents here
-];
+// const botUserAgents = [
+//   'Twitterbot',
+//   'Googlebot',
+//   'Bingbot',
+//   'FacebookExternalHit',
+//   // Add any other bot user agents here
+// ];
 
-export function middleware(req: NextRequest) {
-  const userAgent = req.headers.get('user-agent');
+// export function middleware(req: NextRequest) {
+//   const userAgent = req.headers.get('user-agent');
 
-  // Skip middleware logic if it's a bot (i.e., let crawlers pass freely)
-  if (botUserAgents.some(agent => userAgent?.includes(agent))) {
-    return NextResponse.next();
-  }
+//   // Skip middleware logic if it's a bot (i.e., let crawlers pass freely)
+//   if (botUserAgents.some(agent => userAgent?.includes(agent))) {
+//     return NextResponse.next();
+//   }
 
-  // Your normal middleware logic here, such as handling cookies
-  const cookieStore = req.cookies;
-  const surveyCompletedCookie = cookieStore.get('surveyCompleted');
+//   // Your normal middleware logic here, such as handling cookies
+//   const cookieStore = req.cookies;
+//   const surveyCompletedCookie = cookieStore.get('surveyCompleted');
   
-  if (!surveyCompletedCookie) {
-    return NextResponse.redirect('/');
-  }
+//   if (!surveyCompletedCookie) {
+//     return NextResponse.redirect('/');
+//   }
   
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: '/result/:path*',
-};
+// export const config = {
+//   matcher: '/result/:path*',
+// };
