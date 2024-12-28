8c3a5e9a5797017ffbef7d30d4881b7490a6517f
Shivang <shivangtips@gmail.com>
2024-12-28 11:40:53 +0530

    fix typos

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     
     <main className="flex min-h-screen flex-col items-center justify-center p-6 md:p-24">
       
-      <h1 className="text-4xl font-bold mb-8 font-mono">Whats your gaming profile ?</h1>
+      <h1 className="text-3xl font-bold mb-8 font-mono">Whats your gaming profile ?</h1>
       <SurveyComponent />
     </main>
   )
