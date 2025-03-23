7853e8b01088f040151c21af8e054887055dff89
Shivang <shivangtips@gmail.com>
2025-03-23 19:38:22 +0530

    Added titlt effect

--- components/Result.tsx
+++ components/Result.tsx
@@ -10,6 +10,8 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import Confetti from "react-confetti";
 import Marquee from "react-fast-marquee";
 
+import Tilt from "react-parallax-tilt";
+
 interface ResultProps {
   archetype: string;
 }
@@ -60,12 +62,12 @@ export const alias: Record<Archetype, string> = {
   Stoic: "Titan",
 };
 const gameImages = [
-  "https://res.cloudinary.com/dekobspwg/image/upload/v1734968061/card-vis_bo9nml.png",
-  "https://res.cloudinary.com/dekobspwg/image/upload/v1734968058/card-guard_1_catlbz.png",
+  "https://res.cloudinary.com/dekobspwg/image/upload/v1742576975/35289427-0e5f-4907-85aa-df08d7af922e_tiowrw.jpg",
   "https://res.cloudinary.com/dekobspwg/image/upload/v1734968061/card-explo_1_wfvygh.png",
   "https://res.cloudinary.com/dekobspwg/image/upload/v1734968050/card-achie_arbq9x.png",
   "https://res.cloudinary.com/dekobspwg/image/upload/v1734968062/card-stra_xwxjn0.png",
   "https://res.cloudinary.com/dekobspwg/image/upload/v1740680136/card-unknown_2_t3ztmz.png",
+  "https://res.cloudinary.com/dekobspwg/image/upload/v1742577496/bf237122-83e6-4588-a556-f92589bdfa82_bzujb0.jpg",
 
   "https://res.cloudinary.com/dekobspwg/image/upload/v1734968060/card-challe_ulwzan.png",
   "https://res.cloudinary.com/dekobspwg/image/upload/v1734968051/card-diplo_cdygvc.png",
@@ -140,7 +142,17 @@ export default function Result({ archetype }: ResultProps) {
 
     return () => clearTimeout(timer);
   }, [username, getUsernameFromURL]);
-
+  const defaultOptions = {
+    reverse: false, // reverse the tilt direction
+    max: 20, // max tilt rotation (degrees)
+    perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+    scale: 1.1, // 2 = 200%, 1.5 = 150%, etc..
+    speed: 1000, // Speed of the enter/exit transition
+    transition: true, // Set a transition on enter/exit.
+    axis: null, // What axis should be disabled. Can be X or Y.
+    reset: true, // If the tilt effect has to be reset on exit.
+    easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
+  };
   return (
     <>
       <div className="fixed inset-0 overflow-hidden -z-10 opacity-20">
@@ -202,23 +214,24 @@ export default function Result({ archetype }: ResultProps) {
             The {alias[archetype as Archetype]}
           </p>
           <div className="relative w-full h-32 sm:w-full sm:h-60 mb-2 mx-auto">
-            <Image
-              src={imageUrl}
-              alt={`${archetype} archetype`}
-              layout="fill"
-              objectFit="cover"
-              className="rounded-md"
-              onLoadingComplete={() => setIsImageLoaded(true)}
-            />
-            {ImageUrl && (
-              <div className="absolute bottom-1 right-1 z-10">
-                <img
-                  src={ImageUrl}
-                  className="size-8 sm:size-12 rounded-md border-2 border-white/80 shadow-purple-600 shadow-sm"
-                  alt="Profile"
-                />
-              </div>
-            )}
+            <Tilt>
+              {" "}
+              <img
+                src={imageUrl}
+                alt={`${archetype} archetype`}
+                className="rounded-md w-full h-full object-cover"
+                onLoad={() => setIsImageLoaded(true)}
+              />
+              {ImageUrl && (
+                <div className="absolute bottom-1 right-1 z-10">
+                  <img
+                    src={ImageUrl}
+                    className="size-8 sm:size-12 rounded-md border-2 border-white/80 shadow-purple-600 shadow-sm"
+                    alt="Profile"
+                  />
+                </div>
+              )}
+            </Tilt>
             <button
               onClick={handleDownload}
               disabled={!isImageLoaded}
